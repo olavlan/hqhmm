bb5386273853f42f5a51d1795a397da30b91bb37
Olav Landsverk <olav.landsverk@gmail.com>
2024-07-31 20:21:12 +0200

    remove unused files

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,6 @@ def create_app(config_object=None):
     app = Flask(__name__)
     app.config.from_object("app.default_settings")
 
-    # set up db
-    # with app.app_context():
-    #     from . import db
-
-    #     db.init()
-
     from .base import get_files_path
 
     os.makedirs(get_files_path(app), exist_ok=True)
